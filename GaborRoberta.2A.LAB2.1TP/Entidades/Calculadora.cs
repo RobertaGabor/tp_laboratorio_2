82ccb9b0f1f4bf8b948034e4e288ef6adf547454
Your Name <you@example.com>
2020-09-10 20:34:29 -0300

    algo

---
+++ GaborRoberta.2A.LAB2.1TP/Entidades/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        private static string ValidarOperador(char operador)
+        {
+            string validated="+";
+            if(operador=='+'|| operador == '-' || operador == '*' || operador == '/')
+            {
+                validated = (Convert.ToString(operador));
+            }
+            return validated;
+        }
+
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+            char op = Convert.ToChar(operador);
+            operador = Calculadora.ValidarOperador(op);
+            Console.WriteLine(operador);
+
+            return 12;
+        }
+
+
+    }
+}
