abe93e0fda555b1012efdd1ed65cf3b93ccf3976
Your Name <you@example.com>
2020-09-11 00:17:42 -0300

    calculadora proceso

--- GaborRoberta.2A.LAB2.1TP/Entidades/Calculadora.cs
+++ GaborRoberta.2A.LAB2.1TP/Entidades/Calculadora.cs
@@ -20,10 +20,9 @@ namespace Entidades
 
         public static double Operar(Numero num1, Numero num2, string operador)
         {
-            char op = Convert.ToChar(operador);
             
-            operador = Calculadora.ValidarOperador(op);
-            Console.WriteLine(operador);
+
+
             /*EN PROCESO*/
             
             return 12;
