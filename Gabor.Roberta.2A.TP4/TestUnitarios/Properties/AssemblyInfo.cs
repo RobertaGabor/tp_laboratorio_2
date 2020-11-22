0a8f2477175a0ebddadd0b796b75214c21613ba6
Roberta <robyrubirex@gmail.com>
2020-11-22 01:48:49 -0300

    test unitarios creados

---
+++ Gabor.Roberta.2A.TP4/TestUnitarios/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestUnitarios")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestUnitarios")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f1ff106-836a-4c88-bb22-4d6879477867")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Gabor.Roberta.2A.TP4/TestUnitarios/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using Excepciones;
+
+namespace TestUnitarios
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [ExpectedException(typeof(dniInvalidoException))]
+        public void DNI_InvalidoArgentino_Alto()
+        {
+            string dni = "hola";
+
+            Jugador player = new Jugador(dni);
+        }
+        [TestMethod]
+        public void IgualdadJugadores()
+        {
+            Jugador uno = new Jugador(12);
+            Jugador dos = new Jugador(11);
+
+            bool rta = uno == dos;
+            Assert.IsFalse(rta);
+        }
+        [TestMethod]
+        public void Agregar_JugadorCasino()
+        {
+            Casino casino = new Casino();
+            Jugador player = new Jugador(1);
+            casino += player;
+            casino += player;
+
+            Assert.AreEqual(casino.Jugadores.Count, 1);
+
+        }
+    }
+}
